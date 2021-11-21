1ba8c74b29f2512625527818c5778ec427d055ef
Brent Labasan <BrentLabasan@gmail.com>
2021-11-21 14:47:57 -0800

    SUCCESS slider for opacity balance between the 2 layers works !!

--- src/components/Layer.js
+++ src/components/Layer.js
@@ -2,7 +2,7 @@ import './Layer.css';
 
 function Layer(props) {
   return (
-    <div className="mapLayer">
+    <div className="mapLayer" style={{ opacity: props.opacity }}>
       This is a layer {props.numbLayer}
       <br/>
       opacity is {props.opacity}
